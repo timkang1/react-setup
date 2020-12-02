9af6d907bd37aa58bf4d29e215e9cee84f6a1ce1
timkang93 <jeongmo.kang@gmail.com>
2020-12-02 15:43:12 -0800

    complete click counter without server

---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+(() => {
+  console.log('webpack worked')
+})()
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
