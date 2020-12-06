f544a2603ce31b381a04d9e4d940fa0d9d259a19
timkang93 <jeongmo.kang@gmail.com>
2020-12-06 12:41:50 -0800

    [refactor] implement React hooks with localStorage to keep state after page reload

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const initialCount = () => Number(window.localStorage.getItem('count') || 0); // initialCount is a function instead of a value because that function will only be run on the first render. You don't want React to look into localStorage everytime it renders.
+  const [count, setCount] = useState(initialCount);
 
-  return (
+  // React will call the callback inside useEffect ONLY when the count value changes
+  useEffect(() => {
+    window.localStorage.setItem('count', count);
+  }, [count]);
+
+  return(
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me!
-      </button>
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Click</button>
     </div>
   );
 }
-
-export default App;
